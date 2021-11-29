1d458cda39c0e189ecebd4fb16dc85d596660629
kirillSkorodelov <kirillSkorodelov@users.noreply.github.com>
2021-11-29 11:42:33 +0000

    Initial setup
    Initial setup

---
+++ formsJS/selectTemplateForm_OBS_Pack01.js
@@ -0,0 +1,20 @@
+var creditForm_OBS_Pack01 =
+{
+  "schema": {
+    "field": {
+      "id": "line1",
+      "type": "string",
+      "title": "Your text for credit line:",
+      "name": "input1"
+    }
+  },
+  "form": [
+    {
+      "key": "field"
+    },
+
+    
+  ]
+}
+
+
